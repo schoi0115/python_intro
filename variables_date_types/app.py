7acdb89f6fb86ecc42426f023172d8f6f4777c0f
Seungil <schoi0115@gmail.com>
2021-11-10 00:14:43 -0500

    functions

--- variables_date_types/app.py
+++ variables_date_types/app.py
@@ -1,5 +1,6 @@
 character_name = "John"
 character_age = "35"
+isMale = True
 
 print("There once was a man named " + character_name +", ")
 print("he was " + character_age + " years old. ")
@@ -8,3 +9,4 @@ character_name = "Shawn"
 
 print("He really liked the name " + character_name +" ")
 print("but didn't like being " + character_age + ".")
+print(isMale)
---
+++ working_with_strings/app.py
@@ -0,0 +1,20 @@
+print("Giraffe\"Academy") #\ is escape
+print("Giraffe\nAcademy") #Change Line
+
+phrase = "Giragge Academy"
+print(phrase + "is cool")
+
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())  #using two function in same time
+print(len(phrase))  #Using Lenght function
+print(phrase[0]) # Grabbing
+print(phrase.index("i")) # Where it starts
+print(phrase.index("Academy")) #Finding where "A" starts at
+print(phrase.replace("Giragge", "Elephant")) # First place for taget, second perameter what to change
+
+
+
+phrase = ["Shawn", "Barbara"]
+print(phrase[0])
